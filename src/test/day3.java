a73746772b824bc0f29f6d21e8f20f1e92c13838
Okles-PC\Okles <l.oklesinski@gmail.com>
2018-02-12 20:34:37 +0100

    updated a file tree

---
+++ src/test/day3.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day3 {
+
+	@Test
+	public void webLoginCarLoan()
+	{
+		
+		//selenium
+		System.out.println("Web login car");
+		
+	}
+	
+	@Test
+	public void mobileLoginCarLoan()
+	{
+		
+		//appium
+		System.out.println("Mobile login car");
+		
+	}
+	
+	@Test
+	public void loginAPIcarLoan()
+	{
+		
+		//REST API automation
+		System.out.println("REST of things ");
+		
+	}
+	
+	
+}
---
+++ src/test/day4.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day4 {
+
+	@Test
+	public void webLoginhomeLoan()
+	{
+		
+		//selenium
+		System.out.println("Web login home");
+		
+	}
+	
+	@Test
+	public void mobileLoginhomeLoan()
+	{
+		
+		//appium
+		System.out.println("Mobile login home");
+		
+	}
+	
+	@Test
+	public void loginAPIhomeLoan()
+	{
+		
+		//REST API automation
+		System.out.println("REST of things ");
+		
+	}
+	
+	
+}
